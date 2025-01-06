60e8aa8ccbd9d296d99b94a9d69935d56578da18
DmiTsa <dmitrytaskaev.dt@gmail.com>
2025-01-06 19:47:47 +0300

    feat: stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,14 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
- * @param {List} l
+ * @param {ListNode} l
  * @param {Number} k
- * @return {List}
+ * @return {ListNode}
  *
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
@@ -22,11 +21,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let filtredNode
+  let current = l
+
+  while (current) {
+
+
+    current = current.next
+  }
+  return l
 }
 
 module.exports = {
   removeKFromList
 };
+
+// const list = {
+//   value: 3,
+//   next: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: {
+//             value: 5,
+//             next: null
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// const kt = 3
+
+// console.log(removeKFromList(list, kt)) // => [1, 2, 4, 5]
